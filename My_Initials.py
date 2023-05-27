e0a0eb7d93b35cdc334ec0f8283b9d2837b598cf
Michael J. Medina <121559364+MichaelJMedina@users.noreply.github.com>
2023-05-28 00:16:16 +0900

    Add files via upload

---
+++ My_Initials.py
@@ -0,0 +1,11 @@
+# My name is Michael Medina
+# I am the author of this program
+name = """
+M         M  M         M
+MM       MM  MM       MM
+M M     M M  M M     M M
+M  M   M  M  M  M   M  M
+M   M M   M  M   M M   M
+M    M    M  M    M    M
+"""
+print(name)
